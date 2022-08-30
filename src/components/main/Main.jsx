d430b02b884099c27475fdf35d877d676cc3e59d
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2022-08-30 14:04:34 +0300

    fix

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import AboutUs from './components/AboutUs';
 import HowItWorks from './components/HowItWorks';
 import Reviews from './components/Reviews';
 import MainPageHeaderAndForm from './components/MainPageHeaderAndForm';
 
-class Main extends Component {
-	render() {
-		return (
-			<div>
-				<MainPageHeaderAndForm />
-				<AboutUs />
-				<HowItWorks />
-				<Reviews />
-			</div>
-		);
-	};
+function Main() {
+	return (
+		<div>
+			<MainPageHeaderAndForm />
+			<AboutUs />
+			<HowItWorks />
+			<Reviews />
+		</div>
+	);
 };
 
 export default Main;
