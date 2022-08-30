c870ee7c75a41e5d7598caee64a46dbd10ab6037
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2022-08-30 14:07:09 +0300

    fix

--- src/components/passengers/components/SectionPassengers.jsx
+++ src/components/passengers/components/SectionPassengers.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import progressStateSelect from '../../../images/progress_state_select.png';
 import progressStateSelected from '../../../images/progress_state_selected.png';
 import progressStateDefault from '../../../images/progress_state_default.png';
@@ -12,80 +12,75 @@ import { connect } from 'react-redux';
 import { setPersonInfoAC } from '../../../redux/action';
 
 
-class SectionPassengers extends React.Component {
+function SectionPassengers(props) {
 
-	state = {
-		disableForm: true,
-		activeButton: true,
-		personInfo: {}
-	};
+	const [disableForm, setDisableForm] = useState(true);
+	const [activeButton, setActiveButton] = useState(true);
+	const [personInfo, setPersonInfo] = useState({});
 
-	setDisableForm = () => this.setState({ disableForm: true });
+	const handleDisableForm = () => setDisableForm(true);
 
-	setActiveButton = () => this.setState({ activeButton: false, disableForm: false });
-
-	setPersonInfo = (data, number) => {
-		this.setState({ personInfo: data });
-		this.props.setPersonInfo(data, number);
-	};
-
-	setPersonData = () => {
-		this.props.history.push("/pay_selection");
-		this.props.setPersonInfo(this.state.personInfo);
-	};
-
-
-	render() {
-
-		const passengersFormJSX = this.props.seatsNumbers.map((el, i) => {
-			return <PassengerForm
-				key={el}
-				passengerNumber={i + 1}
-				setPersonInfo={this.setPersonInfo}
-				setActiveButton={this.setActiveButton}
-				activeForm={i === 0}
-				disableForm={this.state.disableForm}
-				setDisableForm={this.setDisableForm} />
-		});
-
-		return (
-			<div>
-				<ProgressLineCost tickets={progressStateSelected}
-					passengers={progressStateSelect}
-					passengersClass="completed"
-					payment={progressStateDefault}
-					paymentClass=""
-					checkClass="" />
-
-				<div className="container d-flex flex-wrap">
-					<SideBarPassangersSection />
-
-					<div className="col mb-5">
-
-						{passengersFormJSX}
-
-						<NavLink className="box-add-passenger mt-5 mb-5" to="/seat_selection">
-							<div className="passengers-form-filling col">
-								<div className="passengers-form-number row pt-4 pb-4">
-									<h5 className="ml-3 mt-1">Добавить пассажира</h5>
-									<img className="ml-auto mr-5 mt-auto" src={iconPlusSmallYellow} alt="..." />
-								</div>
-							</div>
-						</NavLink>
-
-						<div className="d-flex justify-content-end">
-							<button
-								className="btn btn-warning text-white font-weight-bold pl-5 pr-5 mt-5 mb-5"
-								type="button"
-								onClick={this.setPersonData}
-								disabled={this.state.activeButton}>Далее</button>
-						</div>
-					</div>
-
-				</div>
-			</div>
-		);
+	const handleActiveButton = () => {
+		setActiveButton(false); 
+		setDisableForm(false);
 	}
+
+	const handlePersonInfo = (data, number) => {
+		setPersonInfo({data});
+		props.setPersonInfo(data, number);
+	};
+
+	const handlePersonData = () => {
+		props.history.push("/pay_selection");
+		props.setPersonInfo(personInfo);
+	};
+
+	return (
+		<div>
+			<ProgressLineCost tickets={progressStateSelected}
+				passengers={progressStateSelect}
+				passengersClass="completed"
+				payment={progressStateDefault}
+				paymentClass=""
+				checkClass="" />
+
+			<div className="container d-flex flex-wrap">
+				<SideBarPassangersSection />
+
+				<div className="col mb-5">
+
+					{props.seatsNumbers.map((el, i) => {
+						return <PassengerForm
+							key={el}
+							passengerNumber={i + 1}
+							setPersonInfo={handlePersonInfo}
+							setActiveButton={handleActiveButton}
+							activeForm={i === 0}
+							disableForm={disableForm}
+							setDisableForm={handleDisableForm} />
+					})}
+
+					<NavLink className="box-add-passenger mt-5 mb-5" to="/seat_selection">
+						<div className="passengers-form-filling col">
+							<div className="passengers-form-number row pt-4 pb-4">
+								<h5 className="ml-3 mt-1">Добавить пассажира</h5>
+								<img className="ml-auto mr-5 mt-auto" src={iconPlusSmallYellow} alt="..." />
+							</div>
+						</div>
+					</NavLink>
+
+					<div className="d-flex justify-content-end">
+						<button
+							className="btn btn-warning text-white font-weight-bold pl-5 pr-5 mt-5 mb-5"
+							type="button"
+							onClick={handlePersonData}
+							disabled={activeButton}>Далее</button>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	);
 };
 
 const mapStateToProps = (state) => {
