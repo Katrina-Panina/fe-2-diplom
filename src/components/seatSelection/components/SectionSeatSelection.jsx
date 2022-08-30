69be1ec6c7eee8006ea12bae7521446f74fd0b2c
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2022-08-30 14:25:40 +0300

    Migrate SectionSeatSelection to hooks

--- src/components/seatSelection/components/SectionSeatSelection.jsx
+++ src/components/seatSelection/components/SectionSeatSelection.jsx
@@ -11,26 +11,21 @@ import TrainTicket from './TrainTicket';
 
 import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { useState } from 'react';
 
+function SectionSeatSelection(props) {
+	const [disabledButton, setDisableButton] = useState(true);
 
-
-class SectionSeatSelection extends React.Component {
-
-	state = {
-		disabledButton: true
+	const setActiveButton = (value) => {
+		setDisableButton(value);
 	}
 
-	setActiveButton = (value) => {
-		this.setState({ disabledButton: value });
-	}
-
-	setRouteTrainSeatReducer = () => {
+	const setRouteTrainSeatReducer = () => {
 		window.scrollTo(0, 700);
-
-		this.props.history.push('/passengers');
+		props.history.push('/passengers');
 	}
 
-	trainButtonFrom =
+	const trainButtonFrom =
 		<div className="choice-other-train-button d-flex flex-wrap mt-4">
 			<button type="button" className="btn btn-outline-light p-0 ml-3"><img src={choiceOtherTrainButtonThere} alt="..." /></button>
 			<NavLink type="button"
@@ -38,76 +33,73 @@ class SectionSeatSelection extends React.Component {
 				to="/search_tickets">Выбрать другой поезд</NavLink>
 		</div>;
 
-	trainButtonTo =
+	const trainButtonTo =
 		<div className="choice-other-train-button d-flex flex-wrap justify-content-end mt-4">
 			<button type="button" className="btn btn-outline-light p-0"><img src={choiceOtherTrainButtonBack} alt="..." /></button>
 			<NavLink type="button" className="btn btn-outline-dark ml-3 mr-3 pl-5 pr-5 pt-3 font-weight-bold"
 				to="/search_tickets">Выбрать другой поезд</NavLink>
 		</div>;
 
-	render() {
+	const ticketSelected = props.ticketsArray.find(el => el.departure._id === props.trainId);
+	let fromDateTime = ticketSelected.departure.from.datetime;
+	let toDateTime = ticketSelected.departure.to.datetime;
+	let duration = ticketSelected.departure.duration;
+	let fromArrival = fromDateTime + duration;
+	let toArrival = toDateTime + duration;
 
-		const ticketSelected = this.props.ticketsArray.find(el => el.departure._id === this.props.trainId);
-		let fromDateTime = ticketSelected.departure.from.datetime;
-		let toDateTime = ticketSelected.departure.to.datetime;
-		let duration = ticketSelected.departure.duration;
-		let fromArrival = fromDateTime + duration;
-		let toArrival = toDateTime + duration;
+	return (
+		<div>
+			<ProgressLineCost tickets={progressStateSelect}
+				passengers={progressStateDefault}
+				passengersClass=""
+				payment={progressStateDefault}
+				paymentClass=""
+				checkClass="" />
 
-		return (
-			<div>
-				<ProgressLineCost tickets={progressStateSelect}
-					passengers={progressStateDefault}
-					passengersClass=""
-					payment={progressStateDefault}
-					paymentClass=""
-					checkClass="" />
+			<div className="container d-flex flex-wrap">
+				<SideBarSearchTicketsAndSeatSelection />
 
-				<div className="container d-flex flex-wrap">
-					<SideBarSearchTicketsAndSeatSelection />
+				<div className="choice-of-place col pt-5 pb-5">
+					<h3 className="text-uppercase">выбор мест</h3>
 
-					<div className="choice-of-place col pt-5 pb-5">
-						<h3 className="text-uppercase">выбор мест</h3>
+					<TrainTicket
+						trainButton={trainButtonFrom}
+						trainName={ticketSelected.departure.train.name}
+						cityNameDeparture={ticketSelected.departure.from.city.name}
+						cityNameArrival={ticketSelected.departure.to.city.name}
+						railwayStationDeparture={ticketSelected.departure.from.railway_station_name}
+						railwayStationArrival={ticketSelected.departure.to.railway_station_name}
+						dateTime={fromDateTime}
+						arrivalTime={fromArrival}
+						duration={duration}
+						iconSearch={iconSearchThere}
+						setActiveButton={setActiveButton} />
 
-						<TrainTicket
-							trainButton={this.trainButtonFrom}
-							trainName={ticketSelected.departure.train.name}
-							cityNameDeparture={ticketSelected.departure.from.city.name}
-							cityNameArrival={ticketSelected.departure.to.city.name}
-							railwayStationDeparture={ticketSelected.departure.from.railway_station_name}
-							railwayStationArrival={ticketSelected.departure.to.railway_station_name}
-							dateTime={fromDateTime}
-							arrivalTime={fromArrival}
-							duration={duration}
-							iconSearch={iconSearchThere}
-							setActiveButton={this.setActiveButton} />
+					<TrainTicket
+						trainButton={trainButtonTo}
+						trainName={ticketSelected.departure.train.name}
+						cityNameDeparture={ticketSelected.departure.to.city.name}
+						cityNameArrival={ticketSelected.departure.from.city.name}
+						railwayStationDeparture={ticketSelected.departure.to.railway_station_name}
+						railwayStationArrival={ticketSelected.departure.from.railway_station_name}
+						dateTime={toDateTime}
+						arrivalTime={toArrival}
+						duration={duration}
+						iconSearch={iconSearchBack}
+						setActiveButton={setActiveButton} />
 
-						<TrainTicket
-							trainButton={this.trainButtonTo}
-							trainName={ticketSelected.departure.train.name}
-							cityNameDeparture={ticketSelected.departure.to.city.name}
-							cityNameArrival={ticketSelected.departure.from.city.name}
-							railwayStationDeparture={ticketSelected.departure.to.railway_station_name}
-							railwayStationArrival={ticketSelected.departure.from.railway_station_name}
-							dateTime={toDateTime}
-							arrivalTime={toArrival}
-							duration={duration}
-							iconSearch={iconSearchBack}
-							setActiveButton={this.setActiveButton} />
-
-						<div className="d-flex justify-content-end">
-							<button
-								className="btn btn-warning text-white font-weight-bold pl-5 pr-5 mt-5 mb-5"
-								type="button"
-								disabled={this.state.disabledButton}
-								onClick={this.setRouteTrainSeatReducer}
-							>Далее</button>
-						</div>
+					<div className="d-flex justify-content-end">
+						<button
+							className="btn btn-warning text-white font-weight-bold pl-5 pr-5 mt-5 mb-5"
+							type="button"
+							disabled={disabledButton}
+							onClick={setRouteTrainSeatReducer}
+						>Далее</button>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 const mapStateToProps = (state) => {
