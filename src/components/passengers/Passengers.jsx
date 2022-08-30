13758444b140ac0c1f052e1c3c18ada520a1a8ae
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2022-08-30 14:13:59 +0300

    fix

--- src/components/passengers/Passengers.jsx
+++ src/components/passengers/Passengers.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import SectionSearchTicketsHeader from '../shared/HeaderSearchForm';
 import SectionPassengers from './components/SectionPassengers';
 
-class Passengers extends Component {
-	render() {
-		return (
-			<div>
-				<SectionSearchTicketsHeader />
-				<SectionPassengers />
-			</div>
-		);
-	};
+function Passengers() {
+	return (
+		<div>
+			<SectionSearchTicketsHeader />
+			<SectionPassengers />
+		</div>
+	);
 };
 
 export default Passengers;
